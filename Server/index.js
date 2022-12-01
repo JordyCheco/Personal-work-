d7c236ec393bf64bb8765d04ebb10045175836ef
JordyCheco <112654260+JordyCheco@users.noreply.github.com>
2022-12-01 18:20:58 -0500

    connected to mongo DB

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,6 @@
-const express = require('express')
-const app = express()
-
-const productsController = require('./controllers/products');
-
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
-
-app.use('/',express.static);
-
-app
-.get('/', (req, res) => {
-    res.status(200).send('Happy Sweet New Year');
-})
-.use('/api/v1/products', productsController)
-
-
-
-app.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+const express = require("express");
+const connection = require("./mongo");
+const app = express();
+app.listen(3000, () => {
+  console.log("Listening at port 3000");
 });
