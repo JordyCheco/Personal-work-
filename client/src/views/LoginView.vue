7f85c2bb81cf88d51c3e3c50a61a8b6f17729b26
JordyCheco <112654260+JordyCheco@users.noreply.github.com>
2022-12-09 01:53:04 -0500

    fixed up login

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -31,14 +31,6 @@
         Sign In
       </button>
 
-
-      
-      <div
-        class="alert alert-warning alert-dismissible fade show mt-5 d-none"
-        role="alert"
-        id="alert_1"
-      >
-      </div>
     </form>
   </div>
 </article>
